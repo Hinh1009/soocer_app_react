510c4c0dfa7b8bc7cc1d8768ac17a35854311af1
Hinh1009 <dinhhinh10091998@gmail.com>
2020-08-27 10:19:48 +0700

    update final

--- src/Routes.js
+++ src/Routes.js
@@ -3,9 +3,6 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
 class Routes extends Component {
-    constructor(props) {
-        super(props)
-    }
 
     render() {
         return (
